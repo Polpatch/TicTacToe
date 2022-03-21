6bb4e574515deddac847da3c248f21a2080afb86
Alessio Murru <murru.ale91@gmail.com>
2022-03-21 22:28:53 +0100

    handled click on modal

--- Assets/Scripts/ModalInfoController.cs
+++ Assets/Scripts/ModalInfoController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ModalInfoController : MonoBehaviour
@@ -32,6 +33,11 @@ public class ModalInfoController : MonoBehaviour
         _instance = this;
 
         animator = GetComponent<Animator>();
+
+        Button btn = GetComponent<Button>();
+
+        btn.onClick.AddListener(CloseModal);
+        
     }
 
     public void OpenModal(string text, CallbackFunc callback){
